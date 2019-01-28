bef82973f19376c39a58a5f3445473dd7d3fdc72
zhagyilig <zhagyilig@gmail.com>
2019-01-28 11:33:39 +0800

    update shell format:octopus:

--- shell/mysql_backup.sh
+++ shell/mysql_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 db_user="root"
 db_passwd="Lisx_new_123"
 db_host="localhost"
@@ -22,21 +23,23 @@ test ! -d "$backup_dir/backup.0/" && $MKDIR "$backup_dir/backup.0/"
 all_db="$($MYSQL -u $db_user -h $db_host -p$db_passwd -Bse 'show databases')"
 for db in $all_db
 do
-$MYSQLDUMP -u $db_user -p$db_passwd --skip-lock-tables --opt --add-drop-database --add-drop-table --events --triggers --routines --default-character-set=utf8 --master-data=2  --single-transaction --complete-insert --quote-names --log-error=$backup_dir/mysqldump_db_$time.log $db | $GZIP -9 > "$backup_dir/backup.0/$time.$db.gz"
+    $MYSQLDUMP -u $db_user -p$db_passwd --skip-lock-tables --opt --add-drop-database --add-drop-table --events --triggers --routines --default-character-set=utf8 --master-data=2  --single-transaction --complete-insert --quote-names --log-error=$backup_dir/mysqldump_db_$time.log $db | $GZIP -9 > "$backup_dir/backup.0/$time.$db.gz"
 done
 
 $MYSQLDUMP -uroot -p$db_passwd --all-databases --opt --add-drop-database --add-drop-table --events --triggers --routines --default-character-set=utf8 --master-data=2  --single-transaction --complete-insert --quote-names --log-error=$backup_dir/mysqldump$time.log | $GZIP -9 > "$backup_dir/backup.0/$time.all_mysql"
 
 # delete the oldest backup
 test -d "$backup_dir/backup.7/" && $RM -rf "$backup_dir/backup.7"
+
 # rotate backup directory
 for int in 6 5 4 3 2 1 0
 do
-if(test -d "$backup_dir"/backup."$int")
-then
-next_int=`expr $int + 1`
-$MV "$backup_dir"/backup."$int" "$backup_dir"/backup."$next_int"
-fi
+    if(test -d "$backup_dir"/backup."$int")
+    then
+        next_int=`expr $int + 1`
+    $MV "$backup_dir"/backup."$int" "$backup_dir"/backup."$next_int"
+    fi
 done
+
 exit 0;
 
