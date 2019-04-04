920b4dce812cc8cea6a0c1e305dc1fee07535474
zhagyilig <zhagyilig@gmail.com>
2019-04-04 15:48:24 +0800

    delete aws.

--- python/16-imss/src/instance_info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 查询实例的tag和内网ip
-
-while read line
-do
-    aws ec2 describe-instances --filter Name=tag:Name,Values=$line --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value|[0],PrivateIpAddress]' --output text  --profile lsg >> hostname.txt
-done <  instance_tag.txt
